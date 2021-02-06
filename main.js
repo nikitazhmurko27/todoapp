45d33026a4074f3341a5372c4c12bcda476e378d
nikita_zhmurko <n.zhmurko27@gmail.com>
2021-02-06 22:59:44 +0200

    add new task pop-up markup

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 $(function(){
+	
+	//disable all inputs 
+	function enableDisableInputs(){
+		if ($('.new-task').hasClass('active')) {
+			console.log('123');
+			//set opacity to main window(.list)
+			$('.list').css('opacity', '0.7');
+			$('#header-search').prop('disabled', true);
+			$('#filter-status').prop('disabled', true);
+			$('#filter-priotity').prop('disabled', true);
+		}else{
+			$('.list').css('opacity', '1');
+			$('#header-search').prop('disabled', false);
+			$('#filter-status').prop('disabled', false);
+			$('#filter-priotity').prop('disabled', false);
+		}
+	}
 
-	console.log("test");
+	//open "add new task" pop-up
+	$("#add_new").click(function(){
+		if (!$('.new-task').hasClass('active')){
+			$('.new-task').addClass('active');
+			enableDisableInputs();
+		}
+	});
 
+	//cancel "add new task" pop-up
+	$("#new-task-cancel").click(function(){
+		if ($('.new-task').hasClass('active')){
+			$('.new-task').removeClass('active');
+			enableDisableInputs();
+		}
+	});
 });

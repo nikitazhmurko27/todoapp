8554df293196eecd281cc99a59bce4d1bceae94b
nikita_zhmurko <n.zhmurko27@gmail.com>
2021-02-06 23:37:35 +0200

    add new task function

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ $(function(){
 	//disable all inputs 
 	function enableDisableInputs(){
 		if ($('.new-task').hasClass('active')) {
-			console.log('123');
 			//set opacity to main window(.list)
 			$('.list').css('opacity', '0.7');
 			$('#header-search').prop('disabled', true);
@@ -17,19 +16,57 @@ $(function(){
 		}
 	}
 
+	function hideErrorMsg(){
+		if ($('.errorMessage').hasClass('active')){
+			$('.errorMessage').removeClass('active');
+		}
+	}
 	//open "add new task" pop-up
-	$("#add_new").click(function(){
+	$('#add_new').click(function(){
 		if (!$('.new-task').hasClass('active')){
 			$('.new-task').addClass('active');
 			enableDisableInputs();
 		}
 	});
 
-	//cancel "add new task" pop-up
-	$("#new-task-cancel").click(function(){
+	function hideAddNew(){
 		if ($('.new-task').hasClass('active')){
 			$('.new-task').removeClass('active');
 			enableDisableInputs();
 		}
+	}
+	//cancel "add new task" pop-up
+	$('#new-task-cancel').click(function(){
+		hideAddNew();
 	});
+
+	//create new task
+	$('#new-task-create').click(function(){
+		let title = $('#title').val();
+		let priority = $('#priority option:selected').val();
+		if (title.length > 0 && priority != 0) {
+			let tasksCount = parseInt($('.task').length);
+			let taskId = tasksCount+1;
+			let taskMarkup = "<div class='task' data-id='"+taskId+"' data-status='to_do' data-priority='"+priority+"'>";
+					taskMarkup+= "<div class='task-status'>";
+						taskMarkup+= "<input type='checkbox' id='tb"+taskId+"'><label for='tb"+taskId+"'></label>";
+					taskMarkup+= "</div>";
+					taskMarkup+= "<div class='task-content'>";
+						taskMarkup+="<p class='task-content-title'>"+title+"</p>";
+						taskMarkup+="<p class='task-content-priority'>"+priority+"/10</p>";
+					taskMarkup+= "</div>";
+					taskMarkup+= "<div class='task-delete'>";
+						taskMarkup+="<img class='delete-img' src='delete.svg'>";
+					taskMarkup+= "</div>";
+				taskMarkup+="</div>";
+			$('.list-content').append(taskMarkup);
+			hideAddNew();
+		}else{
+			if (!$('.errorMessage').hasClass('active')){
+				$('.errorMessage').addClass('active');
+				setTimeout(hideErrorMsg, 2000);
+			}
+		}
+	});
+	
 });
